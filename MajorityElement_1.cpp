a202da88d665da6580656bd2ada42f84b2d60fdc
SHIVA KRISHNA KOVID KOTARU <93729900+KovidKrishnan@users.noreply.github.com>
2023-06-15 06:47:42 +0530

    Create MajorityElement_1.cpp

---
+++ MajorityElement_1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int majority_element = 0,count = 0;
+        for(int i=0;i<nums.size();i++){
+            if(count == 0){
+                majority_element = nums[i];
+                count++;
+            }
+            else if(majority_element == nums[i]) count++;
+            else count--;
+        }
+        return majority_element;
+    }
+};
